64c26dac63172ac5ff5dc3442abf6eaec4eeaa2f
pavelMerlin <sparkypablos@gmail.com>
2023-03-02 12:20:02 +0200

    Новій функціонал в2.3

--- task5/triangle.py
+++ task5/triangle.py
@@ -1,4 +1,4 @@
-import random
+import time
 from triangle_logic import *
 from data import *
 
@@ -19,7 +19,7 @@ def detect_words(re_input_side, re_input_deg):
             try:
                 re_input_side[i] = float(re_input_side[i])
             except:
-                print("Ошибка ввода сторон")
+                print("\033[31m\033[4mОшибка ввода сторон\033[0m")
                 re_input_deg.clear()
                 re_input_side.clear()
                 main_screen()
@@ -36,7 +36,7 @@ def detect_words(re_input_side, re_input_deg):
                     break
                 re_input_deg[i] = float(re_input_deg[i])
             except:
-                print("Ошибка ввода углов")
+                print("\033[31m\033[4mОшибка ввода углов\033[0m")
                 re_input_deg.clear()
                 re_input_side.clear()
                 main_screen()
@@ -56,12 +56,39 @@ def main_screen():
     user_name = random.choice(user_names)
     user_rang = random.choice(user_rangs)
 
-    user_input_side = input(f"\033[37mПривет \033[31m{user_rang} {user_name}\033[37m\n"
+    user_input_side = input(f"\n\033[37mПривет \033[31m{user_rang} {user_name}\033[37m\n"
                             f"Тут мы считаем возможность существования твоем мамы\n"
-                            f"Введи стороны треугольника в формате (сторона A, сторона B или сторона C)\n")
+                            f"Введи стороны треугольника в формате (сторона A, сторона B или сторона C или H для "
+                            f"ручного ввода)\n"
+                            f">>> ")
+
+    if user_input_side == 'H':
+        user_input_side = user_input_side.replace("H", "")
+        print("\nРучной ввод сторон\n"
+              "Введите указаную сторону или пропустите нажав Enter\n")
+        for i in ["A", "B", "C"]:
+            hand_input = input(f"Введите сторону {i} или Enter\n"
+                               f">>> ")
+            if i == "C":
+                user_input_side += f"{hand_input}"
+            elif hand_input != "":
+                user_input_side += f"{hand_input} "
 
     user_input_deg = input(f"Введи углы треугольника в формате (угол Aльфа, угол Бета или угол Джета(сигма)) "
-                           f"или нажми Enter\n")
+                           f"или нажми Enter или H для ручного ввода\n"
+                           f">>> ")
+
+    if user_input_deg == 'H':
+        user_input_deg = user_input_deg.replace("H", "")
+        print("\nРучной ввод сторон\n"
+              "Введите указаный угол или пропустите нажав Enter\n")
+        for i in ["A", "B", "J"]:
+            hand_input = input(f"Введите угол {i} или Enter\n"
+                               f">>> ")
+            if i == "J":
+                user_input_deg += f"{hand_input}"
+            elif hand_input != "":
+                user_input_deg += f"{hand_input} "
 
     re_input_side = user_input_side.split(" ")
     re_input_deg = user_input_deg.split(" ")
@@ -78,38 +105,43 @@ def solve():
     solve_dic = []
 
     if len(sides) == 2 and len(degs) == 1:
-        solve_dic.append("\nВведи 1 - Вы можете решить по теореме косинусов\n")
+        solve_dic.append("\t\033[0mВведи 1 - Вы можете решить по теореме косинусов\033[37m\n")
 
     if len(sides) == 1 and len(degs) == 2:
-        solve_dic.append("\nВведи 2 - Вы можете решить по теореме синусов\n")
+        solve_dic.append("\t\033[0mВведи 2 - Вы можете решить по теореме синусов\033[37m\n")
 
     if len(sides) == 2 and len(degs) == 1:
         if degs[0] >= 90:
-            solve_dic.append("\nВведи 3 - Вы можете решить по теореме Пифагора\n")
+            solve_dic.append("\t\033[0mВведи 3 - Вы можете решить по теореме Пифагора\033[37m\n")
 
-    if len(sides) == 3 and len(degs) == 0:
-        solve_dic.append("\nВведи 4 - Вы можете найти периметр\n")
-        solve_dic.append("\nВведи 5 - Вы можете найти все углы в треугольнике\n")
+    if len(sides) == 3 and len(degs) >= 0:
+        solve_dic.append("\t\033[0mВведи 4 - Вы можете найти периметр\033[37m\n")
+        solve_dic.append("\t\033[0mВведи 5 - Вы можете найти все углы в треугольнике\033[37m\n")
 
-    if len(sides) == 3 and len(degs) == 0:
-        solve_dic.append("\nВведи 6 - Вы можете найти высоту\n")
+    if len(sides) == 3 and len(degs) >= 0 and len(h) != 1:
+        solve_dic.append("\t\033[0mВведи 6 - Вы можете найти высоту\033[37m\n")
 
-    print("Как вы хотите решить задачу?\n"
-          "\tДано:")
+    print("\nДано:")
     for i in range(len(sides)):
-        print(f"{i} сторона равна {sides[i]}")
+        print(f"\033[37m{i + 1}) сторона равна \033[31m{sides[i]}\033[37m")
     for i in range(len(degs)):
-        print(f"{i} угол равен {degs[i]}")
+        print(f"\033[37m{i + 1}) угол равен \033[31m{degs[i]}\033[37m")
+    for i in range(len(h)):
+        print(f"\033[37mВысота равена \033[31m{round(h[i], 2)}\033[37m")
 
-    print("Как вы хотите решить задачу?\nИсходя из Ваших данных вот возможные действия\n\n")
+    time.sleep(2)
+
+    print("\nКак вы хотите решить задачу?\nИсходя из Ваших данных вот возможные действия\n\n")
+
+    time.sleep(3)
+
     for i in solve_dic:
         print(i)
-
     if len(solve_dic) == 0:
-        print("\nС Вашими данными невозможно корректно выполнить ни одну операцию\n")
+        print("\n\033[31m\033[4mС Вашими данными невозможно корректно выполнить ни одну операцию\n\033[37m")
         main_screen()
 
-    user_choice = input()
+    user_choice = input(">>> ")
 
     if user_choice == '1':
         a_b_cosj()
@@ -124,13 +156,14 @@ def solve():
     elif user_choice == '6':
         height()
     else:
-        print("\nНевозможно\n")
-        main_screen()
+        print("\n\033[0mНевозможно\n")
+        solve()
 
 
 if __name__ == '__main__':
+    main_screen()
+    user_data = False
     for _ in range(100):
-        main_screen()
 
         triangle_existence()
         solve()
--- task5/triangle_logic.py
+++ task5/triangle_logic.py
@@ -1,5 +1,6 @@
 import random
 import math
+import time
 from data import *
 from triangle import *
 
@@ -8,23 +9,23 @@ def triangle_existence():
     if len(sides) == 3:
         existence = (sides[0] + sides[1]) - sides[2]
         if existence <= 0:
-            print("Такой треугольник не может существовать!!!\n")
+            print("\033[31mТакой треугольник не может существовать!!!\n")
             main_screen()
 
     if len(degs) == 3:
         info = sum(degs) != 180
         if info is True:
-            print(f"Сумма градусов введена не верно {sum(degs)}\n")
+            print(f"\033[31mСумма градусов введена не верно {sum(degs)}\n")
             main_screen()
 
     if len(sides) > 3 or len(degs) > 3:
-        print("Ошибка ввода")
+        print("\033[31mОшибка ввода\n")
         main_screen()
     elif len(sides) == 0:
-        print("Ошибка ввода")
+        print("\033[31mОшибка ввода\n")
         main_screen()
     elif len(sides) == 1 and len(degs) < 0:
-        print("Ошибка ввода")
+        print("\033[31mОшибка ввода\n")
         main_screen()
 
 
@@ -35,11 +36,17 @@ def a_sina_sinb():
 
     b = sides[0] * math.sin(math.radians(degs[1])) / math.sin(math.radians(degs[2]))
 
+    c = sides[0]
+
     print("Стороны треуголки равны:\n"
           f"\t A: \t{round(a, 2)}\n"
           f"\t B: \t{round(b, 2)}\n"
-          f"\t C: \t{sides[0]}\n")
+          f"\t C: \t{round(c, 2)}\n")
     print(f"\tУгол Альфа равен \t{degs[2]}\n")
+    sides[0] = a
+    sides.append(b)
+    sides.append(c)
+    time.sleep(2)
 
 
 def deg_find():
@@ -49,30 +56,35 @@ def deg_find():
 
     cos_c = (sides[1] ** 2 + sides[2] ** 2 - sides[0] ** 2) / (2 * sides[2] * sides[1])
 
-    print(f"{round(math.degrees(math.acos(cos_a)), 2)} угол А\n"
-          f"{round(math.degrees(math.acos(cos_b)), 2)} угол B\n"
-          f"{round(math.degrees(math.acos(cos_c)), 2)} угол J\n")
+    print(f"{degs.append(round(math.degrees(math.acos(cos_a))), 2)} угол А\n"
+          f"{degs.append(round(math.degrees(math.acos(cos_b))), 2)} угол B\n"
+          f"{degs.append(round(math.degrees(math.acos(cos_c))), 2)} угол J\n")
+    time.sleep(2)
 
 
 def a_b_cosj():
     c = math.sqrt(pow(sides[0], 2) + pow(sides[1], 2) - 2 * sides[0] * sides[1] * math.cos(math.radians(degs[0])))
+
     sides.append(round(c, 2))
     print("Стороны треуголки равны:\n"
           f"\t A: \t{sides[0]}\n"
           f"\t B: \t{sides[1]}\n"
-          f"\t C: \t{sides[2]}\n")
+          f"\t\033[37mC: \t{sides[2]}\033[0m\n")
     print(f"\tУгол Альфа равен \t{degs[0]}\n")
+    time.sleep(2)
 
 
 def perimetr():
     p = sides[0] + sides[1] + sides[2]
     print(p)
+    time.sleep(2)
 
 
 def height():
     p = (sides[0] + sides[1] + sides[2]) / 2
-    h = (2 * math.sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]))) / sides[0]
-    print(f"Высота равна: {round(h, 2)}\n")
+    h.append((2 * math.sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]))) / sides[0])
+    print(f"Высота равна: {round(*h, 2)}\n")
+    time.sleep(2)
 
 
 def pifagor():
@@ -91,4 +103,4 @@ def pifagor():
     else:
         print("Вы ввели опцию некорректно")
         main_screen()
-
+    time.sleep(2)
